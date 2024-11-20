b00d07441c0a05657b64fad1d0b51d0da908cf3f
tiiakeskinarkaus <tiia.keskinarkaus@centria.fi>
2024-11-20 19:12:55 +0200

    eka commit

---
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.firstapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.firstapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.mainActivityTitleText.text = "Sininen"
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.mainActivityButton.setOnClickListener {
+            binding.mainActivityTitleText.text = "Keltainen"
+        }
+    }
+}
